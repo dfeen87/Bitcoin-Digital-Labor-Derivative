aa0ab4ef691e9b06df4124c293ebff40050cb641
Don Michael Feeney Jr. <dfeen87@gmail.com>
2026-02-05 20:56:53 -0500

    Add Bitcoin Core RPC chain data source

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 pub mod alerts;
 pub mod economic_oracle;
 pub mod rbi_engine;
+pub mod utxo_scoring;
+pub mod velocity_analyzer;
+pub mod velocity_config;
+
+#[cfg(feature = "rpc")]
+pub mod bitcoin_core_chain;
 
 pub mod prelude;
